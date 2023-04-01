a8916499a71c10711cb14da8fc68ab9a451c55c6
chatka <xui@xui.ru>
2023-04-01 19:42:04 +0300

    added 'goods' app to settings and created Category and SubCategory models

--- shop/goods/models.py
+++ shop/goods/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to='category_images/')
+
+    def __str__(self):
+        return self.name
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to='category_images/')
+    parent_category = models.ForeignKey(Category, related_name='subcategories', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.parent_category.name} - {self.name}"
